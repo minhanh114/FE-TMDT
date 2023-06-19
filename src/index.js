632e53aea2c3d8144a9820f0df90e9b1e9211df8
tran minh anh <anh.tran@beetech.com.vn>
2023-06-19 17:08:34 +0700

    fix latest

--- src/index.js
+++ src/index.js
@@ -9,11 +9,19 @@ import store from './store'
 
 import { positions, transitions, Provider as AlertProvider } from 'react-alert';
 import AlertTemplate from 'react-alert-template-basic'
+import Cookies from 'js-cookie';
 
 const options = {
   timeout: 3000,
   position: positions.BOTTOM_RIGHT,
   transition: transitions.SCALE
+}
+
+// Kiểm tra xem đã có token trong cookie hay chưa
+const token = Cookies.get('token');
+if (token) {
+  // Lưu token vào Redux store (hoặc nơi lưu trữ khác) để có thể sử dụng trong ứng dụng
+  store.dispatch({ type: 'SET_TOKEN', payload: token });
 }
 
 ReactDOM.render(
