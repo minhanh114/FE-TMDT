12e8317da3bff8d2c3f5ca0245a7a05677421eed
tran minh anh <anh.tran@beetech.com.vn>
2023-06-19 18:18:19 +0700

    fix latest

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -68,7 +68,7 @@ export const login = (email, password) => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: LOGIN_FAIL,
-            payload: error.response.data.message
+            payload: ""
         })
     }
 }
