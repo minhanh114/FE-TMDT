6f56bd7ee3a54ee432abf3a9c9f015ce1507d18e
tran minh anh <anh.tran@beetech.com.vn>
2023-06-19 17:49:19 +0700

    fix latest

--- src/store.js
+++ src/store.js
@@ -3,11 +3,10 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { productsReducer, newProductReducer, productReducer, productDetailsReducer, newReviewReducer, productReviewsReducer, reviewReducer } from './reducers/productReducers'
-import { authReducer, userReducer, forgotPasswordReducer, allUsersReducer, userDetailsReducer, authToken } from './reducers/userReducers'
+import { authReducer, userReducer, forgotPasswordReducer, allUsersReducer, userDetailsReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { newOrderReducer, myOrdersReducer, orderDetailsReducer, allOrdersReducer, orderReducer, orderDetailsIdReducer } from './reducers/orderReducers'
 import { newCategoryReducer, categoryReducer, categoryUDReducer, categoryDetailsReducer } from './reducers/categoryReducers';
-import { getCategoryDetails } from './actions/categoryActions';
 
 const reducer = combineReducers({
     products: productsReducer,
@@ -33,7 +32,6 @@ const reducer = combineReducers({
     category: categoryReducer,
     udCategory: categoryUDReducer,
     categoryDetails: categoryDetailsReducer,
-    authToken: authToken
 })
 
 
