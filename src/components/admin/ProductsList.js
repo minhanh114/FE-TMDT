9f8a91cbef8e77770e1f3eb39699109beea3efc8
tran minh anh <anh.tran@beetech.com.vn>
2023-06-21 14:04:39 +0700

    push

--- src/components/admin/ProductsList.js
+++ src/components/admin/ProductsList.js
@@ -10,175 +10,174 @@ import { getAdminProducts, deleteProduct, clearErrors } from '../../actions/prod
 import { DELETE_PRODUCT_RESET } from '../../constants/productConstants';
 
 const ProductsList = ({ history }) => {
-  const alert = useAlert();
-  const dispatch = useDispatch();
+    const alert = useAlert();
+    const dispatch = useDispatch();
 
-  const [showDeleteModal, setShowDeleteModal] = useState(false);
-  const [selectedProductId, setSelectedProductId] = useState(null);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [selectedProductId, setSelectedProductId] = useState(null);
 
-  const { loading, error, products } = useSelector((state) => state.products);
-  const { error: deleteError, isDeleted } = useSelector((state) => state.product);
+    const { loading, error, products } = useSelector((state) => state.products);
+    const { error: deleteError, isDeleted } = useSelector((state) => state.product);
 
-  useEffect(() => {
-    dispatch(getAdminProducts());
+    useEffect(() => {
+        dispatch(getAdminProducts());
 
-    if (error) {
-      alert.error(error);
-      dispatch(clearErrors());
-    }
+        if (error) {
+            alert.error(error);
+            dispatch(clearErrors());
+        }
 
-    if (deleteError) {
-      alert.error(deleteError);
-      dispatch(clearErrors());
-    }
+        if (deleteError) {
+            alert.error(deleteError);
+            dispatch(clearErrors());
+        }
 
-    if (isDeleted) {
-      alert.success('Product deleted successfully');
-      history.push('/admin/products');
-      dispatch({ type: DELETE_PRODUCT_RESET });
-    }
-  }, [dispatch, alert, error, deleteError, isDeleted, history]);
+        if (isDeleted) {
+            alert.success('Product deleted successfully');
+            history.push('/admin/products');
+            dispatch({ type: DELETE_PRODUCT_RESET });
+        }
+    }, [dispatch, alert, error, deleteError, isDeleted, history]);
 
-  const setProducts = () => {
-    const data = {
-      columns: [
-        {
-          label: 'ID',
-          field: 'id',
-          sort: 'asc',
-        },
-        {
-          label: 'Tên sản phẩm',
-          field: 'name',
-          sort: 'asc',
-        },
-        {
-          label: 'Giá gốc',
-          field: 'price',
-          sort: 'asc',
-        },
-        {
-          label: 'Giá giảm giá',
-          field: 'price_discount',
-          sort: 'asc',
-        },
-        {
-          label: 'Số lượng còn lại',
-          field: 'stock',
-          sort: 'asc',
-        },
-        {
-          label: 'Hành động',
-          field: 'actions',
-        },
-      ],
-      rows: [],
+    const setProducts = () => {
+        const data = {
+            columns: [
+                {
+                    label: 'ID',
+                    field: 'id',
+                    sort: 'asc',
+                },
+                {
+                    label: 'Tên sản phẩm',
+                    field: 'name',
+                    sort: 'asc',
+                },
+                {
+                    label: 'Giá gốc',
+                    field: 'price',
+                    sort: 'asc',
+                },
+                {
+                    label: 'Giá giảm giá',
+                    field: 'price_discount',
+                    sort: 'asc',
+                },
+                {
+                    label: 'Số lượng còn lại',
+                    field: 'stock',
+                    sort: 'asc',
+                },
+                {
+                    label: 'Hành động',
+                    field: 'actions',
+                },
+            ],
+            rows: [],
+        };
+
+        products.forEach((product) => {
+            data.rows.push({
+                id: product._id,
+                name: product.name,
+                price: `${product.price.toLocaleString()} VNĐ`,
+                price_discount: `${(product.price - product.price * product.discount).toLocaleString()} VNĐ`,
+                stock: product.stock,
+                actions: (
+                    <Fragment>
+                        <Link to={`/admin/product/${product._id}`} className="btn btn-primary py-1 px-2">
+                            <i className="fa fa-pencil"></i>
+                        </Link>
+                        <button
+                            className="btn btn-danger py-1 px-2 ml-2"
+                            onClick={() => {
+                                setSelectedProductId(product._id);
+                                setShowDeleteModal(true);
+                            }}
+                        >
+                            <i className="bi bi-trash3"></i>
+                        </button>
+                    </Fragment>
+                ),
+            });
+        });
+
+        return data;
     };
 
-    products.forEach((product) => {
-      data.rows.push({
-        id: product._id,
-        name: product.name,
-        price: `${product.price.toLocaleString()} VNĐ`,
-        price_discount: `${(product.price - product.price * product.discount).toLocaleString()} VNĐ`,
-        stock: product.stock,
-        actions: (
-          <Fragment>
-            <Link to={`/admin/product/${product._id}`} className="btn btn-primary py-1 px-2">
-              <i className="fa fa-pencil"></i>
-            </Link>
-            <button
-              className="btn btn-danger py-1 px-2 ml-2"
-              onClick={() => {
-                setSelectedProductId(product._id);
-                setShowDeleteModal(true);
-              }}
-            >
-              <i className="bi bi-trash3"></i>
-            </button>
-          </Fragment>
-        ),
-      });
-    });
+    const deleteProductHandler = (id) => {
+        dispatch(deleteProduct(id));
+        setShowDeleteModal(false);
+    };
 
-    return data;
-  };
+    return (
+        <Fragment>
+            <MetaData title={'Tất cả sản phẩm'} />
 
-  const deleteProductHandler = (id) => {
-    dispatch(deleteProduct(id));
-    setShowDeleteModal(false);
-  };
-
-  return (
-    <Fragment>
-      <MetaData title={'Tất cả sản phẩm'} />
-
-      <div className="row">
-        <div className="col-12 col-md-2">
-          <Sidebar />
-        </div>
-
-        <div className="col-12 col-md-10">
-          <Fragment>
-            <h1 className="my-5">Tất cả sản phẩm</h1>
-            <Link to="/admin/product">
-              <button type="button" className="btn btn-primary">
-                Thêm mới
-              </button>
-            </Link>
-
-            {loading ? (
-              <Loader />
-            ) : (
-              <MDBDataTable data={setProducts()} className="px-3" bordered striped hover />
-            )}
-
-            {showDeleteModal && (
-              <div className="modal-backdrop fade show">
-                <div className="modal fade" tabIndex={-1} role="dialog" style={{ display: 'block' }}>
-                  <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                      <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">
-                          Thông báo!
-                        </h5>
-                        <button
-                          type="button"
-                          className="close"
-                          data-dismiss="modal"
-                          aria-label="Close"
-                          onClick={() => setShowDeleteModal(false)}
-                        >
-                          <span aria-hidden="true">×</span>
-                        </button>
-                      </div>
-                      <div className="modal-body">Bạn có muốn xóa không?</div>
-                      <div className="modal-footer">
-                        <button
-                          type="button"
-                          className="btn btn-secondary"
-                          onClick={() => setShowDeleteModal(false)}
-                        >
-                          Hủy
-                        </button>
-                        <button
-                          type="button"
-                          className="btn btn-danger"
-                          onClick={() => deleteProductHandler(selectedProductId)}
-                        >
-                          Xóa
-                        </button>
-                      </div>
-                    </div>
-                  </div>
+            <div className="row">
+                <div className="col-12 col-md-2">
+                    <Sidebar />
                 </div>
-              </div>
-            )}
-          </Fragment>
-        </div>
-      </div>
-    </Fragment>
-  );
+
+                <div className="col-12 col-md-10">
+                    <Fragment>
+                        <h1 className="my-5">Tất cả sản phẩm</h1>
+                        <Link to="/admin/product">
+                            <button type="button" className="btn btn-primary">
+                                Thêm mới
+                            </button>
+                        </Link>
+
+                        {loading ? (
+                            <Loader />
+                        ) : (
+                            <MDBDataTable data={setProducts()} className="px-3" bordered striped hover />
+                        )}
+
+                        {showDeleteModal && (
+                            <div className="modal-backdrop fade show">
+                                <div className="modal-dialog" role="document">
+                                    <div className="modal-content">
+                                        <div className="modal-header">
+                                            <h5 className="modal-title" id="exampleModalLabel">
+                                                Thông báo!
+                                            </h5>
+                                            <button
+                                                type="button"
+                                                className="close"
+                                                data-dismiss="modal"
+                                                aria-label="Close"
+                                                onClick={() => setShowDeleteModal(false)}
+                                            >
+                                                <span aria-hidden="true">×</span>
+                                            </button>
+                                        </div>
+                                        <div className="modal-body">Bạn có muốn xóa không?</div>
+                                        <div className="modal-footer">
+                                            <button
+                                                type="button"
+                                                className="btn btn-secondary"
+                                                onClick={() => setShowDeleteModal(false)}
+                                            >
+                                                Hủy
+                                            </button>
+                                            <button
+                                                type="button"
+                                                className="btn btn-danger"
+                                                onClick={() => deleteProductHandler(selectedProductId)}
+                                            >
+                                                Xóa
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
+
+                    </Fragment>
+                </div>
+            </div>
+        </Fragment>
+    );
 };
 
 export default ProductsList;
